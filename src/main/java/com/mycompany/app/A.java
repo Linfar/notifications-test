4a2ba7715976948f6a5ee0092d7cd0773c565c6f
linfar <andrey.titov@jetbrains.com>
2015-12-11 15:42:51 +0300

    init

---
+++ src/main/java/com/mycompany/app/A.java
@@ -0,0 +1,15 @@
+package com.mycompany.app;
+
+public class A {
+  public static interface Bla {
+     public int doIt(int a);
+  }
+
+  public static void a(Bla bla) {
+     System.out.println(bla.doIt(2));
+  }
+
+  public static void main(String[] args) throws Exception {
+     a(i -> i + 1);
+  }
+}
